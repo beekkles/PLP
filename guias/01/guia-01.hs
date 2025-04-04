b94cea6e77710cd28164bedd9413a8a0ab1fdf31
beekkles <thebulletofcharly@gmail.com>
2025-04-03 22:31:34 -0300

    foldNat

--- guias/01/guia-01.hs
+++ guias/01/guia-01.hs
@@ -1,8 +1,4 @@
 --3-------------------------------------------------------------------------------
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-{-# HLINT ignore "Use map" #-}
-{-# HLINT ignore "Use sum" #-}
-
 sum' :: Num a => [a] -> a
 sum' = foldr1 (+)
 
@@ -42,14 +38,53 @@ elementosEnPosicionesPares (x:_:xs) = x:elementosEnPosicionesPares xs
 entrelazar :: [a] -> [a] -> [a]
 entrelazar = foldr f (const [])
   where
-    f x acc ys = if null ys 
-                    then x : acc [] 
+    f x acc ys = if null ys
+                    then x : acc []
                     else x : head ys : acc (tail ys)
 
 --6--------------------------------------------------------------------------------
+
 recr :: (a -> [a] -> b -> b) -> b -> [a] -> b
 recr _ z [] = z
 recr f z (x : xs) = f x xs (recr f z xs)
 
 sacarUna :: Eq a => a -> [a] -> [a]
 sacarUna e = recr (\x xs acc -> if x == e then xs else x:acc) []
+
+insertarOrdenado :: Ord a => a -> [a] -> [a]
+insertarOrdenado e = recr (\x xs acc -> if x <= e && e <= head xs then x:e:xs else x:acc) []
+
+--7--------------------------------------------------------------------------------
+
+mapPares :: ((a,b) -> c) -> [(a,b)] -> [c]
+mapPares f = foldr (\(x,y) acc -> f (x,y) : acc) []
+
+armarPares :: [a] -> [b] -> [(a,b)]
+armarPares [] _           = []
+armarPares _ []           = []
+armarPares (x:xs) (y:ys)  = (x,y): armarPares xs ys
+
+mapDoble :: (a -> b -> c) -> [a] -> [b] -> [c]
+mapDoble _ [] _           = []
+mapDoble _ _ []           = []
+mapDoble f (x:xs) (y:ys)  = f x y : mapDoble f xs ys
+
+--9--------------------------------------------------------------------------------
+data Nat = Cero | Succ Nat 
+  deriving Show
+
+foldNat :: b -> (b -> b) -> Nat -> b
+foldNat z _ Cero     = z
+foldNat z f (Succ a) = f (foldNat z f a)
+
+sumNat :: Nat -> Nat -> Nat
+sumNat Cero _            = Cero
+sumNat _ Cero            = Cero
+sumNat (Succ a) (Succ b) = foldNat (Succ a) Succ (Succ b)
+
+mulNat :: Nat -> Nat -> Nat
+mulNat a (Succ Cero)     = a
+mulNat a (Succ b)        = sumNat a (mulNat a b)
+
+potNat :: Nat -> Nat -> Nat
+potNat p = foldNat (Succ Cero) (mulNat p)
